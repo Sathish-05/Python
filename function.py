1782a13c2d05b85b30d2abfbe23f071cc6d1452c
K.Sathish <74056043+Sathish-05@users.noreply.github.com>
2022-02-24 11:52:37 +0530

    Update function.py

--- function.py
+++ function.py
@@ -4,11 +4,13 @@ def most_frequent(string):
    return list(char)
 string=list(input()) 
 count=0
+op={}
 char=most_frequent(string)
 for i in char:
     count=0
     for x in string:
          if i==x:
               count+=1
-    print(i,":",count)
+    op.update({i:count})
+print(op)
     
